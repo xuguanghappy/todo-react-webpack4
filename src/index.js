5ff7311b6ab86b333a250773edc229e3384f9a5c
xuguanghappy <277060070@qq.com>
2018-04-07 19:35:53 +0800

    增加了css-loader和style-loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import cat from './assets/cat.jpg'
+import './index.css'
+
 class App extends React.Component {
 	render() {
 		return <div> hello webpack</div>
 	}
 }
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<img src={cat} />, document.getElementById("app"))
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,27 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /(node_modules | bower_components)/,
 				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-react']
-          }
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-react']
+          				}
 				}
+			},
+			{
+  				test: /\.(png|jpg|gif)$/,
+  				use: [
+    					{
+      						loader: "file-loader",
+      						options: {}
+   						 }
+  					]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader'},
+					{ loader: 'css-loader'}
+				]
 			}
 		]
 	}
